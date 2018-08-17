d168c2567ecfcd3f181b6230438da6f110bcdae5
AurelienConte <aconte@outlook.fr>
2018-08-17 10:31:44 +0200

    h

--- api/controllers/PayloadsController.js
+++ api/controllers/PayloadsController.js
@@ -470,10 +470,11 @@ exports.count_payloads = function (req, res)
 
                 var new_Date = sub_date[2] + '-' + sub_date[1] + '-' + sub_date[0] + ' ' + date[1];
                 
-                Payload.findOneAndUpdate({DeviceId: element.DeviceId}, {DateGot: new_Date}, function (err, payload) {
+                Payload.find({DeviceId: element.DeviceId}, function (err, resultat) {
                     if(err)
                         console.log("Error : " + err);
-                    console.log(payload);
+                    resultat.DateGot = new_Date;
+                    resultat.save();
                 });
             }
         });
