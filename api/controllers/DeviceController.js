01ae46451b7c10d6a6d50f6a62f0a3aad533480c
AurelienConte <aconte@outlook.fr>
2018-08-29 15:41:39 +0200

    fix bad downlink

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -71,8 +71,8 @@ exports.create_device = function (req, res)
         min = "0" + min;
 
 
-    newDevice.Created = dd + "/" + mm + "/" + yyyy + " " + hh + ":" + min;
-    newDevice.LastUpdate = dd + "/" + mm + "/" + yyyy + " " + hh + ":" + min;
+    newDevice.Created = new Date(dd + "/" + mm + "/" + yyyy + " " + hh + ":" + min);
+    newDevice.LastUpdate = new Date(dd + "/" + mm + "/" + yyyy + " " + hh + ":" + min);
     newDevice.Name = req.body.Name;
     newDevice.Description = req.body.Description;
     newDevice.GroupId = req.body.GroupId;
