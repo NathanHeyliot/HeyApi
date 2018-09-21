934de89374e15fffe96f964fba22a6ec5daf28eb
AurelienConte <aconte@outlook.fr>
2018-09-21 15:24:58 +0200

    adding optin for sales use

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -177,6 +177,7 @@ exports.read_device = function(req, res) //recupere les details d'un capteur et 
                     res.send(err);
 
                 console.log("DEVICE TYPES  /  found : " + my_type);
+                device.salut = my_type;
                 console.log("RES : " + device);
                 res.json(device);
             });
